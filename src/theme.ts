80d59ef57a79c9d7ca1d04c3afb787ed9ab9f3d3
2dowon <dwon424@gmail.com>
2021-12-02 23:24:26 +0900

    💄  add styles for to do list

--- src/theme.ts
+++ src/theme.ts
@@ -1,17 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "white",
-  accentColor: "#9c88ff",
-  cardBgColor: "transparent",
-  backBtn: "/backBtn_w.png",
-};
-
-export const lightTheme: DefaultTheme = {
-  bgColor: "whitesmoke",
-  textColor: "black",
-  accentColor: "#9c88ff",
-  cardBgColor: "white",
-  backBtn: "/backBtn_b.png",
+  bgColor: "#3F8CF2",
+  boardColor: "#DADFE9",
+  cardColor: "white",
 };

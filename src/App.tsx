ce48e29896e0db1e59fcf338f133a8d248f7e0cd
2dowon <dwon424@gmail.com>
2021-12-02 23:10:36 +0900

    ✨  implement Drag and Drop using react-beautiful-dnd library

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 function App() {
-  return <></>;
+  const onDragEnd = () => {};
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {(magic) => (
+            <ul ref={magic.innerRef} {...magic.droppableProps}>
+              <Draggable draggableId="first" index={0}>
+                {(magic) => (
+                  <li ref={magic.innerRef} {...magic.draggableProps}>
+                    <span {...magic.dragHandleProps}>🔥</span>
+                    One
+                  </li>
+                )}
+              </Draggable>
+              <Draggable draggableId="second" index={1}>
+                {(magic) => (
+                  <li ref={magic.innerRef} {...magic.draggableProps}>
+                    <span {...magic.dragHandleProps}>🔥</span>
+                    Two
+                  </li>
+                )}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
 }
 
 export default App;
